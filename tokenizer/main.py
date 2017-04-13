158f58fc5efa7d0041773068f1dbde87f5734879
Patrick Lewis <patrickml.pml@gmail.com>
2017-04-13 10:11:56 -0400

    Streaming

--- tokenizer/main.py
+++ tokenizer/main.py
@@ -1,9 +1,22 @@
 import nltk
-from nltk.tokenize import word_tokenize
+import json
 
+# Download the punkt tokenizer and load it into nltk
 nltk.download('punkt')
 
-sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
-tokens = word_tokenize(sentence)
+# Streams a json input file for a specific number of lines
+# and returns the number of lines as json documents
+def read_docs(filename, limit):
+    docs = [None] * (limit+1)
+    with open(filename, 'r') as f:
+        for i, line in enumerate(f):
+            if i > limit:
+                break
+            docs[i] = json.loads(line)
+    return docs
 
-print(tokens)
+docs = read_docs('/yelp-data/yelp-reviews.json', 10)
+
+for doc in docs:
+    tokens = nltk.word_tokenize(doc["text"])
+    print(tokens)
